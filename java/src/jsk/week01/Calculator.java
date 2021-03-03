f609c58caa2574660789557f88439a949b3ec19d
o3ojsk <hisunggyu@gmail.com>
2021-03-03 11:48:47 +0900

    Init

---
+++ java/src/jsk/week01/Calculator.java
@@ -0,0 +1,77 @@
+package jsk.week01;
+
+import java.util.*;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+        String input = "1 * 10 + 3 + 4 ";
+//[^0-9-+*/. ]
+        char[] inputChars = input.replace(" ", "").trim().toCharArray();
+
+        Stack<Character> chars = new Stack<>();
+        List<Character> operators = new ArrayList<>();
+        Stack<Double> numbers = new Stack<>();
+
+        for (char c : inputChars) {
+            switch (c) {
+                case '+':
+                    operators.add('+');
+                    chars.push('!');
+                    break;
+                case '-':
+                    operators.add('-');
+                    chars.push('!');
+                    break;
+                case '*':
+                    operators.add('*');
+                    chars.push('!');
+                    break;
+                case '/':
+                    operators.add('/');
+                    chars.push('!');
+                    break;
+                default:
+                    chars.push(c);
+                    break;
+            }
+        }
+
+        StringBuilder temp = new StringBuilder();
+        for (Character c : chars) {
+            if (c != '!') {
+                temp.append(c);
+            } else {
+                numbers.push(Double.valueOf(temp.toString()));
+                temp = new StringBuilder();
+            }
+        }
+        numbers.push(Double.valueOf(temp.toString()));
+        Collections.reverse(numbers);
+
+        double value = 0;
+
+        for (Character oper : operators) {
+            switch (oper) {
+                case '+':
+                    value = numbers.pop() + numbers.pop();
+                    numbers.push(value);
+                    break;
+                case '-':
+                    value = numbers.pop() - numbers.pop();
+                    numbers.push(value);
+                    break;
+                case '*':
+                    value = numbers.pop() * numbers.pop();
+                    numbers.push(value);
+                    break;
+                case '/':
+                    value = numbers.pop() / numbers.pop();
+                    numbers.push(value);
+                    break;
+            }
+        }
+
+        System.out.println("Calculation result : " + value);
+    }
+}
